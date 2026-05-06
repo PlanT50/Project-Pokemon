bfea582abbe2994ce9d01ddc521ba706b16ede2e
PlanT50 <omegadarkxyz@gmail.com>
2026-05-06 13:21:37 -0300

    Teste ajustado||get concertado para conseguir pegar atk e hp devidamente||import duplicado removido

--- src/main/java/com/pokemon/Main.java
+++ src/main/java/com/pokemon/Main.java
@@ -3,29 +3,26 @@ package com.pokemon;
 import com.pokemon.api.PokeApi;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.pokemon.api.PokeApi;
-
 
 
 public class Main {
     public static void main(String[] args){
         PokeApi pokeApi = new PokeApi();
     
-    String pokemonName = "Pikachu";
+    String pokemonName = "pikachu";
     String json = pokeApi.buscarPokemon(pokemonName);
 
     JsonObject pokemon = JsonParser.parseString(json).getAsJsonObject();
 
     String name = pokemon.get("name").getAsString();
     int id = pokemon.get("id").getAsInt();
-    int hp = pokemon.get("hp").getAsInt();
-    int atk = pokemon.get("attack").getAsInt();
+    int hp = pokemon.getAsJsonArray("stats").get(0).getAsJsonObject().get("base_stat").getAsInt();
+    int atk = pokemon.getAsJsonArray("stats").get(1).getAsJsonObject().get("base_stat").getAsInt();
 
     System.out.println("Id: " + id);
     System.out.println("Name: " + name);
     System.out.println("hp: " + hp);
     System.out.println("atk: " + atk);
-
 
     
     }
